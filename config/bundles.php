b812fb099efe2609fc5100913bd9147f714c8132
Łukasz Chruściel <lchrusciel@gmail.com>
2020-11-30 00:05:58 +0100

    Add CORS dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -61,4 +61,5 @@ return [
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
