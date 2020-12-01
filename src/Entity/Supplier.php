41079421bfbbcd392720581e91d199609ce1551a
Łukasz Chruściel <lchrusciel@gmail.com>
2020-12-01 11:28:56 +0100

    Supplier resource in API

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(routePrefix="/admin")
+ * @ORM\Entity
+ * @ORM\Table(name="app_supplier")
+ */
+class Supplier
+{
+    /**
+     * @var int
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
