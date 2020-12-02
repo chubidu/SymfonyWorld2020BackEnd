f81c553ea3a3f7cba2e9ca52bd92426278772226
Łukasz Chruściel <lchrusciel@gmail.com>
2020-12-02 11:07:45 +0100

    Supplier API resource with Product relation

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use App\Entity\Supplier;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity
@@ -14,6 +16,24 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  */
 class Product extends BaseProduct
 {
+    /**
+     * @var Supplier|null
+     * @ORM\ManyToOne(targetEntity="App\Entity\Supplier")
+     * @ORM\JoinColumn(name="supplier_id", referencedColumnName="id")
+     * @Groups({"product:read", "product:create", "product:update"})
+     */
+    private $supplier;
+
+    public function getSupplier(): ?Supplier
+    {
+        return $this->supplier;
+    }
+
+    public function setSupplier(?Supplier $supplier): void
+    {
+        $this->supplier = $supplier;
+    }
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
---
+++ src/Entity/Supplier.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_supplier")
+ * @ApiResource(routePrefix="/admin")
+ */
+class Supplier
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
