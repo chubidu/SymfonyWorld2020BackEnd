be0c7b943989e43a43e5f5dba29b4a61952115b7
Łukasz Chruściel <lchrusciel@gmail.com>
2020-12-02 12:51:21 +0100

    Restrict access to wishlist details

--- src/Doctrine/Extension/WishlistQueryExtension.php
+++ src/Doctrine/Extension/WishlistQueryExtension.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace App\Doctrine\Extension;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Wishlist;
 use Doctrine\ORM\QueryBuilder;
 use Sylius\Component\Customer\Context\CustomerContextInterface;
 
-final class WishlistQueryExtension implements QueryCollectionExtensionInterface
+final class WishlistQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
-    /**
-     * @var CustomerContextInterface
-     */
+    /** @var CustomerContextInterface */
     private $customerContext;
 
     public function __construct(CustomerContextInterface $customerContext)
@@ -28,6 +27,22 @@ final class WishlistQueryExtension implements QueryCollectionExtensionInterface
         string $resourceClass,
         string $operationName = null
     ) {
+        $this->restrictAccessForCustomer($resourceClass, $queryBuilder);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        string $operationName = null,
+        array $context = []
+    ) {
+        $this->restrictAccessForCustomer($resourceClass, $queryBuilder);
+    }
+
+    private function restrictAccessForCustomer(string $resourceClass, QueryBuilder $queryBuilder): void
+    {
         if ($resourceClass !== Wishlist::class) {
             return;
         }
@@ -42,7 +57,6 @@ final class WishlistQueryExtension implements QueryCollectionExtensionInterface
 
         $queryBuilder
             ->andWhere(sprintf('%s.customer = :customer', $rootAlias))
-            ->setParameter('customer', $customer)
-        ;
+            ->setParameter('customer', $customer);
     }
 }
